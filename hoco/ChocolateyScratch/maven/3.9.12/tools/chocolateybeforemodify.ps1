35c8c168db23f0e62fd783795dcd7f700a891aa4
Votre Nom <junnyjoe5@gmail.com>
2026-01-12 19:08:03 +0100

    Remove Chocolatey scratch files from repo

--- hoco/ChocolateyScratch/maven/3.9.12/tools/chocolateybeforemodify.ps1
+++
@@ -1,7 +0,0 @@
-﻿$ErrorActionPreference = 'Stop'
-$toolsDir = Split-Path $MyInvocation.MyCommand.Definition
-$installFolders = Split-Path $toolsDir
-. $toolsDir\helpers.ps1
-
-# Delete any folders from older versions before upgrading or uninstalling
-Remove-PreviousVersions -installFolders $installFolders
